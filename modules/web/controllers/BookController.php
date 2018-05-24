dffbcac11bfa8776930f4902ab5f5754234efeb4
ruoshuiqinmo0 <609392790@qq.com>
2018-05-24 15:17:05 +0800

    bookController.php updated

--- modules/web/controllers/BookController.php
+++ modules/web/controllers/BookController.php
@@ -5,7 +5,7 @@ use Yii;
 
 class BookController extends \yii\web\Controller
 {
-
+	public $modelClass = "app\models\book";
     public function actionImages()
     {
     	$nickname = Yii::$app->request->get('name', '');
